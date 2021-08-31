4f5b8a24d3fb0e3da40857e61ec458b5b9874dae
Joseph <josephmugiwara@gmail.com>
2021-08-30 22:34:56 -0300

    Created auditable entity.

--- src/main/java/com/oliveira/bookstore/publishers/entity/Publisher.java
+++ src/main/java/com/oliveira/bookstore/publishers/entity/Publisher.java
@@ -1,6 +1,7 @@
 package com.oliveira.bookstore.publishers.entity;
 
 import com.oliveira.bookstore.books.entity.Book;
+import com.oliveira.bookstore.entity.Auditable;
 import lombok.Data;
 
 import javax.persistence.*;
@@ -9,7 +10,7 @@ import java.util.List;
 
 @Data
 @Entity
-public class Publisher {
+public class Publisher extends Auditable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
